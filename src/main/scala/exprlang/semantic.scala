55f4d75dfa149446fcc024aec6f80596132595d0
Paolo Picci <paolo.picci@sky.uk>
2018-02-16 20:42:42 +0000

    semantic

--- src/main/scala/exprlang/semantic.scala
+++ src/main/scala/exprlang/semantic.scala
@@ -7,8 +7,14 @@ import monad.Monad
 
 object semantic {
 
-  def add[M[_]](t1: Value, t2: Value)(implicit m: Monad[M]): M[Value] = (t1, t2) match {
+  def semAdd[M[_]](t1: Value, t2: Value)(implicit m: Monad[M]): M[Value] = (t1, t2) match {
     case (Num(x), Num(y)) => m.unitM(Num(x + y))
+    case _ => m.unitM(Wrong)
+  }
+
+  def semApply[M[_]](eFun: Value, eVal: Value)(implicit m: Monad[M]): M[Value] = (eFun, eVal) match {
+    //case (_, Wrong) => m.unitM(Wrong)
+    case (Fun(f),v) => f(v).asInstanceOf[M[Value]] // to fix it returns a Any type
     case _ => m.unitM(Wrong)
   }
 
@@ -18,15 +24,30 @@ object semantic {
       case Con(int) => m.unitM(Num(int))
       case Add(t1, t2) =>
         m.bindM( interpret(t1)(e) ) { eT1 =>
-          m.bindM( interpret(t2)(e) ) { eT2 => add(eT1, eT2)}
+          m.bindM( interpret(t2)(e) ) { eT2 =>
+            semAdd(eT1, eT2)}
         }
       case Lam(name, term) =>
         m.unitM {
           Fun(
             (v: Value) => interpret(term)(bind(name)(v)(e)))
         }
-      case _ => ???
+      case App(t1,t2) =>
+        m.bindM( interpret(t1)(e) ) { eFun =>
+          m.bindM( interpret(t2)(e) ) { eVal =>
+            semApply(eFun, eVal)
+          }
+        }
     }
 
+  def showVal(v: Value): String = v match {
+    case Wrong => "<Wrong>"
+    case Fun(x) => "<Function>"
+    case Num(i) => i.toString
+  }
 
+  def test[M[_]](t: Term)(implicit m: Monad[M]): String =
+    m.showM(m.bindM(
+      interpret(t)(emptyEnv[M]))( x => m.unitM(showVal(x)))
+    )
 }
